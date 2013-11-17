11a1326b8978670b170ef70a39b0865e65fe784d
user <user@domain>
2013-11-17 15:09:18 +0000

    commit from monaco

--- VisualStudioOnlineMonacoDemo/Controllers/HomeController.cs
+++ VisualStudioOnlineMonacoDemo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace VisualStudioOnlineMonacoDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Test from Monaco Editor. Your application description page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
